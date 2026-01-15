2431f3bb320e88c58ec6968355fe03e33b292464
Mahdi Hamidi <info.busitech@proton.me>
2026-01-15 21:44:55 +0000

    Cyber Assessment TASK 2

--- cyber-assessment/1-positive-or-negative.py
+++ cyber-assessment/1-positive-or-negative.py
@@ -2,7 +2,8 @@
 import random
 number = random.randint(-10, 10)
 if number > 0:
-	print(f"{number} is POSITIVE\n")
+	print(f"{number} is positive")
 elif number < 0:
-	print(f"{number} is NEGATIVE\n")
-else: print(f"{number}  is ZERO\n")
+	print(f"{number} is negative")
+else:
+	print(f"{number}  is zero")
